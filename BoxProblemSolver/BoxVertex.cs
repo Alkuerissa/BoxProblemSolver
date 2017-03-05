7c662bd7837a2a9a136417f178e4edbcfff1b70f
Noctiphobia <a.abdelkarim@student.mini.pw.edu.pl>
2017-03-05 20:37:44 +0100

    Complete BoxVertex

--- BoxProblemSolver/BoxVertex.cs
+++ BoxProblemSolver/BoxVertex.cs
@@ -8,6 +8,20 @@ namespace BoxProblemSolver
 {
     public class BoxVertex : Vertex
     {
+		public double Width { get; set; }
+		public double Height { get; set; }
 
-    }
+		public BoxVertex() { }
+
+	    public BoxVertex(double width, double height)
+	    {
+		    Width = width;
+		    Height = height;
+	    }
+
+		public override Vertex Copy()
+		{
+			return new BoxVertex(Width, Height);
+		}
+	}
 }
