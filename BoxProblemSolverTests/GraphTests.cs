e3c6178d09e739d205dec6679745f33c3c54c9eb
Alkuerissa <aleksandra.hernik@op.pl>
2017-05-20 18:11:23 +0200

    Benchmark

--- BoxProblemSolverTests/GraphTests.cs
+++ BoxProblemSolverTests/GraphTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BoxProblemSolver;
 
@@ -41,6 +42,41 @@ namespace BoxProblemSolverTests
         {
             var graph = CreateSimpleGraph();
             var longestPath = graph.FindLongestPath();
+        }
+
+        public double RandomInRange(double min, double max, Random generator)
+        {
+            return min + (max - min) * generator.NextDouble();
+        }
+
+        public List<BoxVertex> GenerateData(int n, double minX = 0.5, double maxX = 20, double minY = 0.5, double maxY = 20)
+        {
+            var vertices = new List<BoxVertex>();
+            Random r = new Random();
+
+            for (int i = 0; i < n; ++i)
+                vertices.Add(new BoxVertex(RandomInRange(minX, maxX, r), RandomInRange(minY, maxY, r)));
+            return vertices;
+        }
+
+        [TestMethod]
+        public void MeanTimeAndResult()
+        {
+            int maxn = 1000, step = 10;
+            double[] times = new double[maxn / step];
+            double[] resultsAsFraction = new double[maxn / step];
+            for (int i = 0; i < maxn / step; i++)
+            {
+                int n = i * step + 1;
+                List<BoxVertex> vertices = GenerateData(n);
+                BoxSolver solver = new BoxSolver(vertices);
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                var result = solver.Run();
+                sw.Stop();
+                times[i] = sw.Elapsed.TotalSeconds;
+                resultsAsFraction[i] = (double)result.Count / n;
+            }
         }
     }
 }
